ba7a343bffe88f180ea5e962d080e7676f889eb2
Dev Felipe Nunes <felipe.oliveira@stellabarros.com.br>
2023-02-13 14:52:38 -0300

    finalizei

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
+import { MyTasksList } from '../components/MyTasksList';
 import { TodoInput } from '../components/TodoInput';
 
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  // const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    //TODO - add new task if it's not empty
   }
 
-  function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+  function handleMarkTaskAsDone(id: number) {
+    //TODO - mark task as done if exists
   }
 
   function handleRemoveTask(id: number) {
@@ -21,23 +26,16 @@ export function Home() {
   }
 
   return (
-    <View style={styles.container}>
-      <Header tasksCounter={tasks.length} />
+    <>
+      <Header />
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
+      <MyTasksList 
         tasks={tasks} 
-        toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        onPress={handleMarkTaskAsDone} 
+        onLongPress={handleRemoveTask} 
       />
-    </View>
+    </>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
